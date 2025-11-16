1c2ced456bf6990e2ae28d1178f946c288366dd3
Tyler Schill <tylerjschill@gmail.com>
2025-11-16 19:57:12 +0000

    done part 1

--- src/debug_elf_header.c
+++ src/debug_elf_header.c
@@ -16,67 +16,172 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    char buf[sizeof(Elf64_Ehdr)];    
+    Elf64_Ehdr hdr;
 
-    long bytes_read = sys_read(fd, buf, sizeof(Elf64_Ehdr));
+    long bytes_read = sys_read(fd, &hdr, sizeof(hdr));
     if (bytes_read < 0) {
         return -1;
     }
 
-    if (buf[0] == 0x7f &&
-        buf[1] == 'E' &&
-        buf[2] == 'L' &&
-        buf[3] == 'F') {
-
+    if (hdr.e_ident[EI_MAG0] == 0x7f &&
+        hdr.e_ident[EI_MAG1] == 'E' &&
+        hdr.e_ident[EI_MAG2] == 'L' &&
+        hdr.e_ident[EI_MAG3] == 'F') {
+        
         // Magic Bytes
-        sys_write(1, "Magic Bytes\n", sizeof("Magic Bytes\n"));
-
-        for (int i = 0; i < 4; i ++) {
-            sys_write(1, &buf[i], sizeof(buf[i]));
+        mini_printf("Magic Bytes: %x %x %x %x\n", hdr.e_ident[EI_MAG0], hdr.e_ident[EI_MAG1], hdr.e_ident[EI_MAG2], hdr.e_ident[EI_MAG3]);
+       
+        // Class (ELF32/ELF64)
+        if (hdr.e_ident[EI_CLASS] == ELFCLASSNONE) {
+            mini_printf("Class: ELFNONE\n");
+        }
+        else if (hdr.e_ident[EI_CLASS] == ELFCLASS32) {
+            mini_printf("Class: ELF32\n");
+        }
+        else {
+            mini_printf("Class: ELF64\n");
+        }
+        
+        // Endianness
+        if (hdr.e_ident[EI_DATA] == ELFDATANONE) {
+            mini_printf("Data: Unkown data format\n");
+        }
+        else if (hdr.e_ident[EI_DATA] == ELFDATA2LSB) {
+            mini_printf("Data: 2's compliment, little endian\n");
+        }
+        else {
+            mini_printf("Data: 2's compliment, big endian\n");
         }
 
-        sys_write(1, "\n", sizeof("\n"));
-
-        // Class (ELF32/ELF64)
-        sys_write(1, "Class\n", sizeof("Class\n"));
-        sys_write(1, &buf[4], sizeof(buf[4]));
-        sys_write(1, "\n", sizeof("\n"));
-
-        // Endianness
-        sys_write(1, "Endianness\n", sizeof("Endianness\n"));   
-        sys_write(1, &buf[5], sizeof(buf[5]));
-        sys_write(1, "\n", sizeof("\n"));
-        
         // OS/ABI
-        sys_write(1, "OS/ABI\n", sizeof("OS/ABI\n"));
-        sys_write(1, &buf[7], sizeof(buf[7]));
-        sys_write(1, "\n", sizeof("\n"));
+        if (hdr.e_ident[EI_OSABI] == ELFOSABI_NONE) {
+            mini_printf("OS/ABI: Unix - System V\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_SYSV) {
+            mini_printf("OS/ABI: Unix - System V\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_HPUX) {
+            mini_printf("OS/ABI: HP-UX\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_NETBSD) {
+            mini_printf("OS/ABI: NetBSD\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_LINUX) {
+            mini_printf("OS/ABI: Linux\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_SOLARIS) {
+            mini_printf("OS/ABI: Solaris\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_IRIX) {
+            mini_printf("OS/ABI: IRIX\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_FREEBSD) {
+            mini_printf("OS/ABI: FreeBSD\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_TRU64) {
+            mini_printf("OS/ABI: Unix - TRU64\n");
+        }
+        else if (hdr.e_ident[EI_OSABI] == ELFOSABI_ARM) {
+            mini_printf("OS/ABI: Arm\n");
+        }
+        else {
+            mini_printf("OS/ABI: Stand-alone (embedded)\n");
+        }
 
-        // Type (EXEC, DYN, etc.)
-        sys_write(1, "Type\n", sizeof("Type\n"));
-        // e_ident[] is 16 bytes long
-        sys_write(1, &buf[16], sizeof(buf[16]));
-        sys_write(1, "\n", sizeof("\n"));
+        // Type
+        if (hdr.e_type == ET_NONE) {
+            mini_printf("Type: Unkown\n");
+        }
+        else if (hdr.e_type == ET_REL) {
+            mini_printf("Type: REL (Relocatable file)\n");
+        }
+        else if (hdr.e_type == ET_EXEC) {
+            mini_printf("Type: EXEC (Executable file)\n");
+        }
+        else if (hdr.e_type == ET_DYN) {
+            mini_printf("Type: DYN (Shared object)\n");
+        }
+        else {
+            mini_printf("Type: CORE (Core file)\n");
+        }
 
         // Machine Architecture
-        sys_write(1, "Machine Architecture\n", sizeof("Machine Architecture\n"));
-        // 18 because e_type is 2 bytes
-        sys_write(1, &buf[18], sizeof(buf[18]));
-        sys_write(1, "\n", sizeof("\n"));
+        switch (hdr.e_machine) {
+            case EM_NONE:
+                mini_printf("Machine: Unknown\n");
+                break;
+            case EM_M32:
+                mini_printf("Machine: AT&T WE 32100\n");
+                break;
+            case EM_SPARC:
+                mini_printf("Machine: Sun Microsystems SPARC\n");
+                break;
+            case EM_386:
+                mini_printf("Machine: Intel 80386\n");
+                break;
+            case EM_68K:
+                mini_printf("Machine: Motorola 68000\n");
+                break;
+            case EM_88K:
+                mini_printf("Machine: Motorola 88000\n");
+                break;
+            case EM_860:
+                mini_printf("Machine: Intel 80860\n");
+                break;
+            case EM_MIPS:
+                mini_printf("Machine: MIPS RS3000\n");
+                break;
+            case EM_PARISC:
+                mini_printf("Machine: HP/PA\n");
+                break;
+            case EM_SPARC32PLUS:
+                mini_printf("Machine: SPARC with enhanced instruction set\n");
+                break;
+            case EM_PPC:
+                mini_printf("Machine: PowerPC\n");
+                break;
+            case EM_PPC64:
+                mini_printf("Machine: PowerPC 64-bit\n");
+                break;
+            case EM_S390:
+                mini_printf("Machine: IBM S/390\n");
+                break;
+            case EM_ARM:
+                mini_printf("Machine: Advanced RISC Machines\n");
+                break;
+            case EM_SH:
+                mini_printf("Machine: Renesas SuperH\n");
+                break;
+            case EM_SPARCV9:
+                mini_printf("Machine: SPARC v9 64-bit\n");
+                break;
+            case EM_IA_64:
+                mini_printf("Machine: Intel Itanium\n");
+                break;
+            case EM_X86_64:
+                mini_printf("Machine: AMD x86-64\n");
+                break;
+            case EM_VAX:
+                mini_printf("Machine: DEC Vax\n");
+                break;
+            default:
+                mini_printf("Machine: Not matching any cases\n");
+                break;
+        }
 
         // Entry Point Address
-        sys_write(1, "Entry Point Address\n", sizeof("Entry Point Addres\n"));
-        sys_write(1, &buf[24], sizeof(buf[24]));
-        sys_write(1, "\n", sizeof("\n"));
+        mini_printf("Entry point address: 0x%x\n", hdr.e_entry);
 
-        // Program and Section Header Info
-        sys_write(1, "Program/Section Header Info\n", sizeof("Program/Section Header Info\n"));
-        sys_write(1, &buf[32], sizeof(buf[32]));
-        sys_write(1, &buf[40], sizeof(buf[40]));
-        sys_write(1, "\n", sizeof("\n"));
+        // Program/Section Header Info
+        mini_printf("Start of program headers: %d (bytes into file)\n", hdr.e_phoff);
+        mini_printf("Start of section headers: %d (bytes into file)\n", hdr.e_shoff);
+        mini_printf("Size of program headers: %d (bytes)\n", hdr.e_phentsize);
+        mini_printf("Number of program headers: %d\n", hdr.e_phnum);
+        mini_printf("Size of section headers: %d (bytes)\n", hdr.e_shnum);
+ 
     }
     else {
-        sys_write(1, "Not a valid Elf", sizeof("Not a valid Elf"));
+        sys_write(1, "Not a valid ELF\n", sizeof("Not a valid ELF\n"));
         return -1;
     }
 
