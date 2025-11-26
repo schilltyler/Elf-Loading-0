1190745555f2b85ae7e58bca04ffcfc8a722a9b4
Tyler Schill <tylerjschill@gmail.com>
2025-11-26 15:48:40 +0000

    started parts 4 and 5

--- src/debug_segments.c
+++ src/debug_segments.c
@@ -11,6 +11,47 @@ int main(int argc, char **argv) {
     const char *filename = argv[1];
 
     // Your solution here!
+    int fd = sys_openat(AT_FDCWD, filename, O_RDONLY);
+    if (fd < 0) {
+        mini_printf("Could not open file\n");
+        return -1;
+    }
+
+    Elf64_Ehdr hdr;
+    long bytes_read = sys_read(fd, &hdr, sizeof(hdr));
+    if (bytes_read < 0) {
+        mini_printf("Unsuccessful elf header read\n");
+        return -1;
+    }
+
+    int offset = sys_lseek(fd, hdr.e_phoff, SEEK_SET);
+    if (offset < 0) {
+        mini_printf("Unsuccessful lseek\n");
+        return -1;
+    }
+
+    size_t size = hdr.e_phnum * sizeof(Elf64_Phdr);
+
+    Elf64_Phdr phdr_table;
+    bytes_read = sys_read(fd, &phdr_table, size);
+    if (bytes_read < 0) {
+        mini_printf("Unsuccessful program header read\n");
+        return -1;
+    }
+
+    for (int i = 0; i < hdr.e_phnum; i ++) {
+        if (phdr_table[i].p_type == PT_LOAD) {
+            // Requested virtual address range
+            
+            // Page-aligned address range
+            
+            // Size in bytes and pages
+
+            // File offset and size
+
+            // BSS/Zero-filled regions (if memsz > filesz)
+
+            // Permissions
 
     return 0;
 }
--- src/mini_loader.c
+++ src/mini_loader.c
@@ -9,6 +9,17 @@
 
 void *read_file_into_memory(const char *path, size_t *size) {
     // Your solution here!
+    int fd = sys_openat(AT_FDCWD, path, O_RDONLY);
+    if (fd < 0) {
+        mini_printf("Could not open file\n");
+        return -1;
+    }
+
+    size_t size = sys_lseek(fd, 0, SEEK_END);
+    if (size < 0) {
+        mini_printf("Error during lseek\n");
+        return -1;
+    }
 }
 
 uintptr_t map_elf(void *elf_data, size_t size) {
